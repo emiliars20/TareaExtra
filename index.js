1ecd0b53d9efbe56dc6b6bcc862459da5990730a
camilacamacho421 <camilacamacho421@gmail.com>
2024-09-03 21:10:43 -0300

    cambios que no funcionaron

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const dragon = 'https://dragonball-api.com/api/characters'
 
 const dragonball = document.getElementById("dragonball");
 
+const inputElement = document.getElementById('input');
+let personajes = [];
+
 function showData(dataArray) {
     // El for itera sobre los elementos del array
     for (const item of dataArray) {
@@ -33,8 +36,8 @@ return res.ok? res.json(): Promise.reject(res); //esto hace que: si la respuesta
 })
 .then(data => {
 // Acceso al array products dentro de data 
-const items = data.items;
-showData(items); // Paso products array para la funcion showData
+personajes = data.items;
+showData(personajes); // Paso products array para la funcion showData
 })
 .catch((err) => {
 console.log(err);
@@ -45,3 +48,28 @@ dragonball.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
 console.log('Operación de fetch completada.'));
 
 
+//--- DESAFIATE ---
+
+
+// La función busqueda toma el valor del input y verifica si está en la lista de items
+function busqueda(value) {
+  if (items.includes(value)) {
+    console.log(`${value} está en la lista.`);
+  } else {
+    console.log(`${value} no está en la lista.`);
+  }
+}
+
+// Agrega un EventListener al input para escuchar el evento 'input'
+inputElement.addEventListener('input', function() {
+  // Llama a la función busqueda con el valor actual del input
+  busqueda(inputElement.value);
+});
+
+input.addEventListener("keyup", (e) =>{
+  const str = e.target.value.toLowerCase();
+  const filtro = personajes.filter(personajes=>{
+    return personajes.name.toLowerCase().includes(str);
+  })
+  showData(filtro);
+})
